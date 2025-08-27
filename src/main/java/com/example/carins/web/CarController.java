4e5d7a26cc6e17a1ff19099e334df925cbe29766
Albert <albertcaprar@gmail.com>
2025-08-28 00:55:43 +0300

    Restructured for clarity

--- src/main/java/com/example/carins/web/CarController.java
+++ src/main/java/com/example/carins/web/CarController.java
@@ -97,6 +97,12 @@ public class CarController {
      */
     @GetMapping("/cars/{carId}/history")
     public List<ClaimDto> getCars(@PathVariable Long carId) {
-        return service.listCarClaims(carId).stream().map(c -> new ClaimDto(c.getId(), c.getCar().getId(), c.getClaimDate(), c.getDescription(), c.getAmount())).toList();
+        return service.listCarClaims(carId).stream().map(
+                c -> new ClaimDto(
+                        c.getId(),
+                        c.getCar().getId(),
+                        c.getClaimDate(),
+                        c.getDescription(),
+                        c.getAmount())).toList();
     }
     }
